b649d3b264cda4f20dd2d44c97da903a3428774b
Johnny Tran <johnnetr@hotmail.com>
2018-08-26 15:36:13 +0700

    added neccessary NPM

---
+++ src/Screens/MainScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class MainScreen extends Component {
+    //dummy data
+
+
+  render() {
+    return (
+        // Three FlatList with horizontal scrolling
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    )
+  }
+}
